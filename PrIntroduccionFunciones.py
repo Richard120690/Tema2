6f5f144a78e0eb46b8471eb5df27593ca57033dc
Richard120690 <80859069+Richard120690@users.noreply.github.com>
2026-03-07 11:47:46 -0600

    Add files via upload

---
+++ PrIntroduccionFunciones.py
@@ -0,0 +1,19 @@
+libro1="El señor de los anillos"
+libro2="Programación con Python para universitarios"
+libro3="El diario de Ana Frank"
+
+
+longitudLibro1=len(libro1)
+longitudLibro2=len(libro2)
+longitudLibro3=len(libro3)
+
+print(libro1)
+print(f"La longitud del título del libro1 es: {longitudLibro1} caracteres")
+
+print(libro2)
+print(f"La longitud del título del libro1 es: {longitudLibro2} caracteres")
+
+print(libro3)
+print(f"La longitud del título del libro1 es: {longitudLibro3} caracteres")
+
+
