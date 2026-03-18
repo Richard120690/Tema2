29b5ff47b94bfb2fcd80203d6fdfd75ff68dd664
Richard120690 <80859069+Richard120690@users.noreply.github.com>
2026-03-17 22:24:19 -0600

    Add files via upload

---
+++ opreradores_aritmeticos.py
@@ -0,0 +1,13 @@
+
+
+def calcular_calorias(car,pro,gra):
+    
+
+    tcar=int(car*4)
+    tpro=int(pro*4)
+    tgra=int(gra*9)
+    total=tcar+tpro+tgra
+    print("Calorías totales:", total)
+
+
+calcular_calorias(10,40,5)
