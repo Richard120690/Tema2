456cfeb4770824e46041fa438e6c9b68a5c8c3ba
Richard120690 <80859069+Richard120690@users.noreply.github.com>
2026-03-17 23:38:35 -0600

    Add files via upload

---
+++ OperadoresAsig_Boo_Com.py
@@ -0,0 +1,34 @@
+def comparar_longitud(palabra1="gato",palabra2="perro"):
+
+    longitud1=len(palabra1)
+    longitud2=len(palabra2)
+
+    lon=longitud1==longitud2
+
+
+    return  lon
+
+
+print('¿Son "gato" y "perro" dos palabras con la misma longitud?',comparar_longitud())
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
